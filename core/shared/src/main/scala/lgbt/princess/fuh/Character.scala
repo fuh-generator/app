79cc53f541c86f81239b481f5b18003eb5b2d9b8
Marissa/Princess <dev@princess.lgbt>
2023-05-05 21:31:29 -0400

    Rename `Character#aliases` to `#additionalNames`

--- core/shared/src/main/scala/lgbt/princess/fuh/Character.scala
+++ core/shared/src/main/scala/lgbt/princess/fuh/Character.scala
@@ -4,8 +4,8 @@ import cats.MonadThrow
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
 
-final case class Character(primaryName: Name, aliases: Name*) {
-  def names: Iterable[Name] = aliases.view.prepended(primaryName)
+final case class Character(primaryName: Name, additionalNames: Name*) {
+  def names: Iterable[Name] = additionalNames.view.prepended(primaryName)
 
   def sources: Sources = {
     val aggregated =
@@ -20,9 +20,9 @@ final case class Character(primaryName: Name, aliases: Name*) {
 
   def resolved[F[_]: MonadThrow](lookup: SourceLookup): F[Character] =
     for {
-      primary <- primaryName.resolved(lookup)
-      others  <- aliases.map(_.resolved(lookup)).sequence
-    } yield Character(primary, others: _*)
+      primary    <- primaryName.resolved(lookup)
+      additional <- additionalNames.map(_.resolved(lookup)).sequence
+    } yield Character(primary, additional: _*)
 }
 
 object Character {
